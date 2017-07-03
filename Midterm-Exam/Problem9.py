59a6e2e6e0a5150cea2cb94307aefb8509f283d1
xianglol <xiang.li@outlook.com>
2017-07-03 01:30:29 -0700

    Update Problem9.py

--- Midterm-Exam/Problem9.py
+++ Midterm-Exam/Problem9.py
@@ -30,3 +30,40 @@ if L1 = [1, 'b', 1, 'c', 'c', 1] and L2 = ['c', 1, 'b', 1, 1, 'c'] then is_list_
 Paste your entire function, including the definition, in the box below. Do not leave any debugging print statements.
 """
 
+# Paste your function here
+def is_list_permutation(L1, L2):
+    '''
+    L1 and L2: lists containing integers and strings
+    Returns False if L1 and L2 are not permutations of each other. 
+            If they are permutations of each other, returns a 
+            tuple of 3 items in this order: 
+            the element occurring most, how many times it occurs, and its type
+    '''
+    # Your code here
+
+    ref = {}
+    
+    r = True     
+    
+    for l in L1:
+        if l in L2 and L1.count(l) == L2.count(l):
+            r = r and True
+        else:
+            r = r and False
+    
+    if len(L1) == 0 and len(L2) == 0:
+        return None, None, None
+        
+    elif len(L1) !=0 and len(L2) == 0:
+        return False
+        
+    elif len(L1) == len(L2) and r == True:
+        for l in L1:
+            if l in L2 and L1.count(l) == L2.count(l):
+                ref[l] = L1.count(l)
+        for e in ref:
+            if ref[e] == max(ref.values()):
+                prem = e, ref[e], type(e)
+                return prem
+    else:
+        return False
