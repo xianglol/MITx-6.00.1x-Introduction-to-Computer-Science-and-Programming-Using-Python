b3113cd7b3ffe1a2233219fb02f823f416d80225
xianglol <xiang.li@outlook.com>
2017-07-03 01:47:48 -0700

    Update Problem7.py

--- Midterm-Exam/Problem7.py
+++ Midterm-Exam/Problem7.py
@@ -2,7 +2,9 @@
 #20.0/20.0 points (graded)
 
 """
-Write a function called dict_invert that takes in a dictionary with immutable values and returns the inverse of the dictionary. The inverse of a dictionary d is another dictionary whose keys are the unique dictionary values in d. The value for a key in the inverse dictionary is a sorted list (increasing order) of all keys in d that have the same value in d.
+Write a function called dict_invert that takes in a dictionary with immutable values and returns the inverse of the dictionary. 
+The inverse of a dictionary d is another dictionary whose keys are the unique dictionary values in d. 
+The value for a key in the inverse dictionary is a sorted list (increasing order) of all keys in d that have the same value in d.
 
 Here are two examples:
 
