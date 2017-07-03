bc5d95cd0abaf2b262bd28e4dd8c4f0c70cb1dda
xianglol <xiang.li@outlook.com>
2017-07-03 01:50:10 -0700

    Update Problem9.py

--- Midterm-Exam/Problem9.py
+++ Midterm-Exam/Problem9.py
@@ -2,7 +2,8 @@
 #20.0 points possible (graded)
 
 """
-Write a Python function that takes in two lists and calculates whether they are permutations of each other. The lists can contain both integers and strings. We define a permutation as follows:
+Write a Python function that takes in two lists and calculates whether they are permutations of each other. 
+The lists can contain both integers and strings. We define a permutation as follows:
 
 the lists have the same number of elements
 list elements appear the same number of times in both lists
@@ -12,7 +13,8 @@ If they are permutations of each other, the function returns a tuple consisting 
 the element occuring the most times
 how many times that element occurs
 the type of the element that occurs the most times
-If both lists are empty return the tuple (None, None, None). If more than one element occurs the most number of times, you can return any of them.
+If both lists are empty return the tuple (None, None, None). 
+If more than one element occurs the most number of times, you can return any of them.
 
 def is_list_permutation(L1, L2):
     '''
@@ -32,17 +34,7 @@ Paste your entire function, including the definition, in the box below. Do not l
 
 # Paste your function here
 def is_list_permutation(L1, L2):
-    '''
-    L1 and L2: lists containing integers and strings
-    Returns False if L1 and L2 are not permutations of each other. 
-            If they are permutations of each other, returns a 
-            tuple of 3 items in this order: 
-            the element occurring most, how many times it occurs, and its type
-    '''
-    # Your code here
-
     ref = {}
-    
     r = True     
     
     for l in L1:
@@ -62,5 +54,6 @@ def is_list_permutation(L1, L2):
             if ref[e] == max(ref.values()):
                 prem = e, ref[e], type(e)
                 return prem
+            
     else:
         return False
